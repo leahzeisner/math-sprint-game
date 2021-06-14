a8dffa49619140b0aba4694a952d6851de0d7f8f
leahzeisner <zeisner.l@northeastern.edu>
2021-06-14 08:47:55 -0400

    updated countdown

--- script.js
+++ script.js
@@ -248,16 +248,19 @@ function populateGamePage() {
 
 // Displays 3, 2, 1, GO!
 function countdownStart() {
-  countdown.textContent = '3';
-  setTimeout(() => {
-    countdown.textContent = '2';
+  let count = 3
+  countdown.textContent = count
+  const timeCountdown = setInterval(() => {
+    count--
+    if (count === 0) {
+      countdown.textContent = 'GO!'
+    } else if (count === -1) {
+      showGamePage()
+      clearInterval(timeCountdown)
+    } else {
+      countdown.textContent = count
+    }
   }, 1000);
-  setTimeout(() => {
-    countdown.textContent = '1';
-  }, 2000);
-  setTimeout(() => {
-    countdown.textContent = 'GO!';
-  }, 3000);
 }
 
 // Navigate from Splash Page to CountdownPage to Game Page
@@ -266,7 +269,6 @@ function showCountdown() {
   splashPage.hidden = true;
   countdownStart();
   populateGamePage();
-  setTimeout(showGamePage, 4000);
 }
 
 // Get the value from selected radio button
